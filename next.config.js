0a3f431f1fd8bd11323e605b874def47ccc67f28
Mustafa <thecreativehorse7@gmail.com>
2025-10-10 01:10:28 -0400

    Configure Next.js for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // appDir is now stable and no longer needs to be in experimental
+  output: 'standalone',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
